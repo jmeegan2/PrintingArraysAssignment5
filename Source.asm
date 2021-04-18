9df950add23a3672f159b27bf78d6c950af906fb
jmeegan2 <jmeegan8@gmail.com>
2021-04-18 01:59:32 -0400

    struggling, need to find out how to display .data arrays

--- Source.asm
+++ Source.asm
@@ -13,8 +13,8 @@ ExitProcess PROTO, dwExitCode:DWORD
 ;Data Segment
 .data
 arrayBYTE SBYTE -1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20 		;These three balues are used to denote the contained binary encoding should be treated as a 'signed' value. 
-arraySWORD SWORD -1		;They are capable of representing a negative integer. PLesase keep in mind that this is only done for programmers
-arraySDWORD	SDWORD 	
+;arraySWORD SWORD -1		comeback to it later 4/18
+arraySDWORD	SDWORD 	-1
 ;CODE SEGMENT
 .code 
 main PROC
@@ -104,8 +104,9 @@ MOVSX EAX, arrayBYTE
 	CALL Crlf
 	;so im gonna change this line then push to github
 ;SWORD array will display -1 to -20 using a static count and WriteDec
-MOVZX EAX, [arraySWORD]
-	CALL WriteBinB
+MOVZX EAX, arraySDWORD
+MOV EBX, Type SDWORD
+	CALL WriteInt
 	CALL Crlf
 	
 	 

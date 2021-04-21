f8ee3a0be8102bcad38a0a9f3dab7e3b1f80bfe3
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-21 17:22:53 -0400

    done finished all array types

--- Source.asm
+++ Source.asm
@@ -13,20 +13,19 @@ ExitProcess PROTO, dwExitCode:DWORD
 ;Data Segment
 .data
 arrayBYTE SBYTE -1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20 		;These three balues are used to denote the contained binary encoding should be treated as a 'signed' value. 
-arrayWORD			WORD	15, 20, 30, 40, 50		
-;arraySDWORD	SDWORD 	-1
+arrayWORD			SWORD	1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+arraySDWORD	SDWORD 	1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
 ;CODE SEGMENT
 .code 
 main PROC
-;just wanna get at least one commit today 
-; SBYTE array displaying -1 to -20, This would be considered an instance where the example is mobing from element to element with offset ferived from the
-;"TYPE" directive
+
+;SBYTE data displayed here 
 
 MOVSX EAX, arrayBYTE
 	MOV EBX, TYPE SBYTE			
 	CALL WriteInt
 	CALL Crlf
-	MOVSX EAX,[arrayBYTE + 1]		;Adding to the array allows you to display the next number in the sequence -1 to -2 in this instance
+	MOVSX EAX,[arrayBYTE + 1]		
 	MOV EBX, TYPE SBYTE			
 	CALL WriteInt
 	CALL Crlf
@@ -102,13 +101,220 @@ MOVSX EAX, arrayBYTE
 	MOV EBX, TYPE SBYTE			
 	CALL WriteInt
 	CALL Crlf
-	;so im gonna change this line then push to github
-;SWORD array will display -1 to -20 using a static count and WriteDec
+	CALL crlf
+	
+;SWORD data displayed here 
 
-	MOVZX EAX, [arrayWORD]
-	CALL WriteDec
+	MOVSX EAX, [arrayWORD]
+	NEG eax
+	CALL WriteInt
 	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 2]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 4]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 6]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 8]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 10]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 12]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 14]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 16]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 18]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 20]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 22]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 24]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 26]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 28]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 30]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 32]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 34]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 36]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOVSX EAX, [arrayWORD + 38]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	call crlf
+
+;SDWORD data displayed here 
+
+	MOV EAX, [arraySDWORD]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	
+	MOV EAX, [arraySDWORD + 4]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 8]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOV EAX, [arraySDWORD + 12]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 16]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 20]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 24]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 28]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 32]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 36]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 40]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 44]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 48]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 52]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 56]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 60]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 64]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 68]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	MOV EAX, [arraySDWORD + 72]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+	
+	MOV EAX, [arraySDWORD + 76]
+	NEG eax
+	CALL WriteInt
+	CALL Crlf
+
+	
+	
+	 
 	 
 INVOKE ExitProcess, 0
 main ENDP
+
+
 END main			;end of program OPCODES
